36a154fea597749db7808460189c0d069f7e7a11
shysofts <shakilcust@gmail.com>
2025-04-28 14:26:45 +0600

    version 0.04-beta

--- basepos/customer/admin.py
+++ basepos/customer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Sale, SaleItem, Customer, Medicine
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Medicine)
+admin.site.register(Sale)
+admin.site.register(SaleItem)
