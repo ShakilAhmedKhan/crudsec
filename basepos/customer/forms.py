f1f7f38db80c12a3724239127d43d0492fccc232
shysofts <shakilcust@gmail.com>
2025-06-30 21:01:19 +0300

    version 0.14-beta, went back to old style of the customer bdate calender

--- basepos/customer/forms.py
+++ basepos/customer/forms.py
@@ -5,21 +5,12 @@ from .models import Customer, Medicine, Sale
 class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
-        fields = ['name', 'email', 'phone', 'address', 'date_of_birth']
+        #fields = '__all__'
+        fields = ['name', 'email', 'phone', 'address','date_of_birth']
         widgets = {
-            'date_of_birth': forms.DateInput(
-                attrs={
-                    'type': 'date',
-                    'placeholder': 'dd/mm/yyyy',
-                    'class': 'form-control'
-                },
-                format='%d/%m/%Y'
-            ),
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
         }
 
-    def __init__(self, *args, **kwargs):
-        super(CustomerForm, self).__init__(*args, **kwargs)
-        self.fields['date_of_birth'].input_formats = ['%d/%m/%Y']
 
 class MedicineForm(forms.ModelForm):
     class Meta:
